a7aa7c9b4e4105742eeba3e22f4b8d9242c8fdd4
amiryorav <108618623+amiryorav@users.noreply.github.com>
2025-04-20 16:24:39 +0300

    Delete dist directory

--- dist/scripts/service-worker.js
+++
@@ -1,2 +0,0 @@
-self.addEventListener("install",e=>{e.waitUntil(caches.open("workout-timer-v1").then(e=>e.addAll(["./","./index.html","./editor.html","./styles/main.css","./styles/variables.css","./styles/player.css","./styles/editor.css"])))}),self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(s=>s||fetch(e.request)))});
-//# sourceMappingURL=service-worker.js.map
